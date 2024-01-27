a3cfb07d058f9467d98960613d73a8e4af09bb59
André Coutinho de Macedo Silva <91290799+AndreCoutinhom@users.noreply.github.com>
2024-01-27 08:02:54 -0300

    Update project_clone_and_build.sh

--- Microsserviços na prática: entendendo a tomada de decisões/Boas-vindas à realidade/project_clone_and_build.sh
+++ Microsserviços na prática: entendendo a tomada de decisões/Boas-vindas à realidade/project_clone_and_build.sh
@@ -2,6 +2,10 @@
 
 git clone --recurse-submodules --remote-submodules git@github.com/CViniciusSDias/alura-ms.git
 
+# Outra maneira de clonar o projeto é usando o comando abaixo
+
+git clone --recursive https://github.com/CViniciusSDias/alura-ms.git
+
 # Para rodar os projetos, primeiro caminha-se para a respectiva pasta, e então rode o comando para compilar os projetos na máquina
 
 cd alura-ms
